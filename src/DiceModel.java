3053ebbcb9b5dab36aa261b1222fe48d473265b8
Markus Salmijärvi <makedin@gmail.com>
2013-03-13 19:05:45 +0200

    added initial implementations of some methods to DiceModel

--- src/DiceModel.java
+++ src/DiceModel.java
@@ -1,19 +1,50 @@
 import java.util.Observable;
+import java.util.Random;
 
 public class DiceModel extends Observable {
 
-  private DieValues values;
-  private int rollsLeft;
+  private static final dieLowerBound = 1;
+  private static final dieUpperBound = 6;
+  private static Random rand = new Random(4);
+
+  private DieValues dice;
+  private int rollsTotal;
+  private int rolledTimes;
+  private boolean locked[];
 
 
+  public int getDieCount(){
+    return dice.values.length;
+  }
 
+  public DieValues getDieValues(){
+    return dice;
+  }
+
+  public boolean canDiceBeLocked(){
+    return ((rollsTotal - rolledTimes) > 0);
+  }
+
+  public void setDieLock(int i, boolean state){
+    locked[i] = state;
+  }
+
+  public void roll(){
+    int i = 0;
+    while (i < dice.values.length){
+      if (!locked[i]){
+        dice.values[i] = rand.nextInt(dieUpperBound) + dieLowerBound;
+      }
+      i++;
+    }
+  }
 
 
   public class DieValues {
 
-    int values[];
+    private int values[];
 
-    public DieValues(int count){
+    private DieValues(int count){
       values = new int[count];
     }
   }
