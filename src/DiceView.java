5ff1496449e4b5c5d8ac6a587e97b77f325c1cda
Markus Salmijärvi <makedin@gmail.com>
2013-03-14 19:58:30 +0200

    created Die-class

--- src/DiceView.java
+++ src/DiceView.java
@@ -1,16 +1,36 @@
 import java.util.Observer;
 import javax.swing.JPanel;
 import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.Icon;
 
 public class DiceView extends Jpanel implements Observer {
-
   private DiceModel model;
   private DiceController controller;
   private DiceModel.DieValues dice;
 
-
   public DiceView(DiceModel dmodel){
     super();
     controller = new DiceController(this, model);
+  }
+
+
+  private class Die extends JComponent {
+    private int maxValue;
+    private int currValue;
+    private icon facet[];
+
+    public Die(int max){
+      maxValue = max;
+      facet = new icon[maxValue];
+
+      int i = 0;
+      while (i <= maxValue){
+        facet[i] = new ImageIcon("images/facet" + (i + 1) + ".png");
+    }
+
+    public setValue(int v){
+      currValue = v;
+    }
   }
 }
