9d6752aee6e04a26fb57b43932bf0799412d7408
Mikko Paukkonen <mikko.paukkonen@gmail.com>
2013-03-15 00:55:01 +0200

    Removed overcomplicated score interface from GameModel.

--- src/GameModel.java
+++ src/GameModel.java
@@ -9,69 +9,6 @@ public class GameModel extends Observable {
     private List<Player> players;
     private Player currentPlayer;
 
-    public class PlayerColumn {
-
-        public String name;
-        public boolean isInTurn;
-        public Cell[] cells;
-
-        public class Cell {
-
-            private ScoreCell cell;
-
-            public int getScore() {
-                Integer score = cell.getScore();
-
-                if (score != null)
-                    return score.intValue();
-
-                int[] dieValues = GameModel.this.diceModel.getDieValues();
-
-                return cell.calculateScore(dieValues);
-            }
-
-            public boolean isFilled() {
-                return cell.getScore() != null;
-            }
-
-            public void mark() {
-                int[] dieValues = GameModel.this.diceModel.getDieValues();
-
-                cell.markScore(dieValues);
-            }
-
-            public Cell(ScoreCell cell) {
-                this.cell = cell;
-            }
-        }
-    }
-
-    public PlayerColumn[] getPlayerColumns() {
-        PlayerColumn[] columns = new PlayerColumn[players.size()];
-
-        for (int i = 0; i < players.size(); ++i) {
-            Player player = players.get(i);
-            PlayerColumn column = new PlayerColumn();
-
-            column.name = player.getName();
-            column.isInTurn = (player == currentPlayer);
-
-            ScoreCell[] cells = player.getScoreCells();
-
-            column.cells = new PlayerColumn.Cell[cells.length];
-
-            for (int j = 0; j < cells.length; ++j) {
-                ScoreCell backingCell = cells[j];
-
-                column.cells[j] = column.new Cell(backingCell);
-            }
-
-            columns[i] = column;
-        }
-
-        return columns;
-    }
-
     public String[] getScoreCellNames() {
         return new ScoreColumn(null).getCellNames();
     }
