bf54192e8b84e8ad29dcd50704eb9cfcab94b908
Mikko Paukkonen <mikko.paukkonen@gmail.com>
2013-03-15 13:44:15 +0200

    Added GameController and GameView.

---
+++ src/GameController.java
@@ -0,0 +1,37 @@
+
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+
+/**
+ *
+ * @author Mikko Paukkonen
+ */
+public class GameController {
+
+    GameModel model;
+    DiceModel diceModel;
+
+    public class MarkAction extends AbstractAction {
+
+        private MarkableScoreCell cell;
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            cell.markScore(GameController.this.diceModel.getDieValues());
+        }
+
+        public MarkAction(MarkableScoreCell cell) {
+            this.cell = cell;
+        }
+    }
+
+    public GameController(GameModel model, DiceModel diceModel) {
+        this.model = model;
+        this.diceModel = diceModel;
+    }
+
+    public Action getMarkAction(MarkableScoreCell cell) {
+        return new MarkAction(cell);
+    }
+}
