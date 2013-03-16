80e8ebd333f65486872e99daa9d1dc3b38aee8ae
Markus Salmijärvi <makedin@gmail.com>
2013-03-16 14:54:09 +0200

    fixed indent in DiceController.java

--- src/DiceController.java
+++ src/DiceController.java
@@ -3,14 +3,14 @@ import java.util.Observable;
 
 public class DiceController implements Observer {
 
-  private DiceModel model;
-  private DiceView view;
+    private DiceModel model;
+    private DiceView view;
 
-  public DiceController(DiceView dView, DiceModel dModel){
-    view = dView;
-    model = dModel;
-  }
+    public DiceController(DiceView dView, DiceModel dModel){
+        view = dView;
+        model = dModel;
+    }
 
-  public void update(Observable o, Object arg){
-  }
+    public void update(Observable o, Object arg){
+    }
 }
