731a3435585ee5b1e03d5b9bf967044621091721
Markus Salmijärvi <makedin@gmail.com>
2013-03-16 14:51:20 +0200

    now uses EasyGridBagLayout

--- src/DiceView.java
+++ src/DiceView.java
@@ -1,62 +1,62 @@
 import java.util.Observer;
+import java.util.Observable;
 import javax.swing.JPanel;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.Icon;
 import javax.swing.JToggleButton;
 import javax.swing.BoxLayout;
+import java.awt.GridBagLayout;
 
-public class DiceView extends Jpanel implements Observer {
-  private DiceModel model;
-  private DiceController controller;
-  private DiceModel.DieValues values;
-  private Die dice;
-  
-  private JButton rollButton;
+public class DiceView extends JPanel implements Observer {
+    private DiceModel model;
+    private DiceController controller;
+    private DiceModel.DieValues values;
+    private Die dice[];
 
-  public DiceView(DiceModel dmodel){
-    super();
-    model = dmodel;
-    controller = new DiceController(this, model);
-    dice = new Die[dmodel.getDieCount];
+    private JButton rollButton;
 
-    int i = dice.length;
-    while (i-- > 0){
-      dice[i] = new Die
+    public DiceView(DiceModel model){
+        super(new GridBagLayout());
+        this.model = model;
+        controller = new DiceController(this, model);
+        dice = new Die[model.getDieCount()];
+
+        int i = dice.length;
+        while (i-- > 0){
+            dice[i] = new Die(model.DIE_MAX_VALUE);
+        }
     }
-  }
 
-  void updated(Observable o, Object arg){
+    public void update(Observable o, Object arg){
+        DiceModel.DieValues values = (DiceModel.DieValues)arg;
 
-    DiceModel.DiceValues values = (DiceModel.DiceValues)arg;
-
-    int i = values.getDieCount();
-    while (i-- > 0){
-
-      dice.setValue(values.value
-
-
+        int i = values.getValueCount();
+        while (i-- > 0){
+            dice[i].setValue(values.valueAt(i));
+            EasyGridBagLayout.addToLayout(this, dice[i], i, 0);
+        }
     }
-  }
 
-  private class Die extends JLabel {
-    private int maxValue;
-    private int currValue;
-    //private icon facet[];
-    super("3");
+    private class Die extends JLabel {
+        private int maxValue;
+        private int currValue;
+        //private icon facet[];
 
-    public Die(int max){
-      maxValue = max;
-      facet = new icon[maxValue];
+        public Die(int max){
+            super("1");
+            maxValue = max;
+            //facet = new icon[maxValue];
 
-      int i = 0;
-      while (i <= maxValue){
-        //facet[i] = new ImageIcon("images/facet" + (i + 1) + ".png");
-      }
+            int i = 0;
+            while (i <= maxValue){
+                //facet[i] = new ImageIcon("images/facet" + (i + 1) + ".png");
+            }
+        }
 
-    public setValue(int v){
-      currValue = v;
-      super.setText(Integer.toString(v);
+        public void setValue(int v){
+            currValue = v;
+            super.setText(Integer.toString(v));
+        }
     }
-  }
 }
