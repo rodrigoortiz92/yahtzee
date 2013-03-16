2a4cc3a82c347a2056429e32ef4f635ba3847447
Markus Salmijärvi <makedin@gmail.com>
2013-03-16 17:16:05 +0200

    constructor's parameters now have sane names

--- src/DiceController.java
+++ src/DiceController.java
@@ -3,18 +3,29 @@ import java.util.Observable;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-public class DiceController implements ActionListener{
+public class DiceController implements ActionListener {
 
     private DiceModel model;
     private DiceView view;
 
-    public DiceController(DiceView dView, DiceModel dModel){
-        view = dView;
-        model = dModel;
+    public DiceController(DiceView view, DiceModel model){
+        this.view = view;
+        this.model = model;
+    }
+
+    public boolean lockable(){
+        return model.canDiceBeLocked();
+    }
+
+    public  boolean rollable(){
+        return model.canDiceBeRolled();
     }
 
     public void actionPerformed(ActionEvent ae){
 
 
     }
+
+
+    public class RollAction extends AbstractAction
 }
