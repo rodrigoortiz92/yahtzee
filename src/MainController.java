5310b4e527127d1b92db0d56ab8d6002389168b9
Mikko Paukkonen <mikko.paukkonen@gmail.com>
2013-03-17 19:28:36 +0200

    Added window dimension save and load.

--- src/MainController.java
+++ src/MainController.java
@@ -1,5 +1,12 @@
 
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
+import java.awt.geom.Dimension2D;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
 import javax.swing.AbstractAction;
 
 /*
@@ -15,9 +22,18 @@ public class MainController implements WindowListener {
     private NewGameAction newGameAction = new NewGameAction();
     private ExitAction exitAction = new ExitAction();
     private SetupController controller;
-    
-    public MainController(SetupController controller){
+    private MainView view;
+    private final String WINDOW_X_KEY = "window.x";
+    private final String WINDOW_Y_KEY = "window.y";
+    private final String WINDOW_WIDTH_KEY = "window.width";
+    private final String WINDOW_HEIGHT_KEY = "window.height";
+    private final String PROPERTY_FILE = "properties";
+
+    public MainController(MainView view, SetupController controller) {
+        this.view = view;
         this.controller = controller;
+
+        loadProperties();
     }
 
     public NewGameAction getNewGameAction() {
@@ -74,9 +90,71 @@ public class MainController implements WindowListener {
             view.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         } else {
             view.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            saveProperties();
         }
 
     }
+
+    private void loadProperties() {
+        Properties properties = new Properties();
+
+        FileInputStream in = null;
+
+        try {
+            in = new FileInputStream(PROPERTY_FILE);
+            properties.load(in);
+            int x = new Integer(properties.getProperty(WINDOW_X_KEY));
+            int y = new Integer(properties.getProperty(WINDOW_Y_KEY));
+            int height = new Integer(properties.getProperty(WINDOW_HEIGHT_KEY));
+            int width = new Integer(properties.getProperty(WINDOW_WIDTH_KEY));
+
+            view.setBounds(x, y, width, height);
+        } catch (FileNotFoundException e) {
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void saveProperties() {
+        Properties properties = new Properties();
+
+        Rectangle bounds = view.getBounds();
+
+        properties.setProperty(WINDOW_X_KEY, Integer.toString(bounds.x));
+        properties.setProperty(WINDOW_Y_KEY, Integer.toString(bounds.y));
+
+        Dimension2D size = view.getSize();
+
+        properties.setProperty(WINDOW_WIDTH_KEY, Integer.toString((int) size.getWidth()));
+        properties.setProperty(WINDOW_HEIGHT_KEY, Integer.toString((int) size.getHeight()));
+
+        FileOutputStream out = null;
+
+        try {
+            out = new FileOutputStream(PROPERTY_FILE);
+            properties.store(out, "---Properties---");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (out != null) {
+                    out.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     @Override
     public void windowClosed(WindowEvent e) {
     }
