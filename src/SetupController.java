58ce05de29522ead451d17762555803368544bcd
Markus Salmijärvi <makedin@gmail.com>
2013-03-19 23:33:52 +0200

    more or less rewrote SetupController

--- src/SetupController.java
+++ src/SetupController.java
@@ -1,8 +1,3 @@
-/**
-*
-*@author Erkki Mattila
-*/
-
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.AbstractAction;
@@ -10,49 +5,20 @@ import java.util.Observer;
 import java.util.Observable;
 
 public class SetupController implements Observer {
-    
+
     SetupView view;
     SetupModel model;
     private NewPlayerAction newPlayerAction = new NewPlayerAction();
     private RemovePlayerAction removePlayerAction = new RemovePlayerAction();
-    
+
     public SetupController(SetupView view, SetupModel model) {
         this.view = view;
         this.model = model;
         this.model.addObserver(this);
     }
-    
-    public void update(Observable o, Object args){
-        System.out.println("SetupController observes!");
-    }
-    
-    public NewPlayerAction getNewPlayerAction() {
-        return newPlayerAction;
-    }
 
-    public class NewPlayerAction extends AbstractAction {
-    
-        public NewPlayerAction(){
-            super("Add player");
-        }
-        
-        public void actionPerformed(ActionEvent e) {
-            model.addPlayer();
-        }
-    }
-    
-    public RemovePlayerAction getRemovePlayerAction() {
-        return removePlayerAction;
-    }
-    
-    public class RemovePlayerAction extends AbstractAction {
-    
-        public RemovePlayerAction(){
-            super("Remove player");
-        }
-        
-        public void actionPerformed(ActionEvent e) {
-            model.removePlayer();
-        }
+    public AddPlayerButtonAction getAddPlayerButtonAction(){
+        return addPlayerButtonAction;
+
     }
 }
